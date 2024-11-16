bbeb3209508812517a56cddb404ff6cc42571600
Abbas-coding <abbasfakhruddin205@gmail.com>
2024-11-16 18:30:42 +0500

    Initial Setup

---
+++ MentalHealthAssistant/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module source.mentalhealthassistant {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens source.mentalhealthassistant to javafx.fxml;
+    exports source.mentalhealthassistant;
+}
