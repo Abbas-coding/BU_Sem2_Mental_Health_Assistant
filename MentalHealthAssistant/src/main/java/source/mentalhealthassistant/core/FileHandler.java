65ac4a62a1e6dc30eefc97ab60cd070ebcda67bb
Abbas-coding <abbasfakhruddin205@gmail.com>
2024-11-19 18:37:35 +0500

    Added File & Database Handler Structure from UML

---
+++ MentalHealthAssistant/src/main/java/source/mentalhealthassistant/core/FileHandler.java
@@ -0,0 +1,18 @@
+package source.mentalhealthassistant.core;
+
+public class FileHandler {
+
+    public FileHandler() {
+        // To be implemented later
+    }
+
+    public void saveUserData(User user){
+        // To be implemented later
+    }
+
+    public void loadUserData(User user){
+        // To be implemented later
+    }
+
+
+}
